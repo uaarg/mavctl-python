db4ab3618edee7c8c2beb825bccd75bf7d73d8c7
UA-24 Raspi 4 <uaarg@raspi>
2025-10-25 20:55:22 +0100

    flight test stuff

--- mavctl/bin/run-nav.sh
+++ mavctl/bin/run-nav.sh
@@ -3,4 +3,4 @@ set -e
 set +x
 source venv/bin/activate
 
-PYTHONUNBUFFERED=1 PYTHONPATH="." python3 tests/flight_tests/first_flight.py | tee --append shepard.log
+PYTHONUNBUFFERED=1 PYTHONPATH="." python3 mavctl/tests/flight_tests/first_flight.py | tee --append shepard.log
--- mavctl/tests/flight_tests/first_flight.py
+++ mavctl/tests/flight_tests/first_flight.py
@@ -5,7 +5,7 @@ from mavctl.messages.messenger import Messenger
 from mavctl.messages import advanced
 import time
 
-CONN_STR = "udp:127.0.0.1:14550"
+CONN_STR = "tcp:127.0.0.1:14550"
 
 mav = Connect(ip = CONN_STR)
 master = Navigator(mav.mav)
@@ -18,6 +18,21 @@ while not master.set_mode_wait():
 
 master.takeoff(10)
 time.sleep(5)
-advanced.simple_goto_global(master, 53.496970, -113.545194, 20)
+advanced.simple_goto_global(master, 53.496533, -113.550414, 20)
+time.sleep(5)
+advanced.simple_goto_global(master, 53.496562, -113.552387, 10)
+time.sleep(5)
+advanced.simple_goto_global(master, 53.496021, -113.545194, 15)
+time.sleep(5)
+advanced.simple_goto_global(master, 53.496173, -113.547501, 20)
+time.sleep(5)
+advanced.simple_goto_local(master, 10, 0, 10)
+time.sleep(5)
+advanced.simple_goto_local(master, 0, 10, 10)
+time.sleep(5)
+advanced.simple_goto_local(master, -10, 0, 10)
+time.sleep(5)
+advanced.simple_goto_local(master, 0, -10, 10)
+time.sleep(5)
 
 master.return_to_launch()
